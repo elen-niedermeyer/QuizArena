8f93e70f381443441b837adea76f2f8b1b317d4e
Elen Niedermeyer <elen.niedermeyer@posteo.de>
2018-03-08 19:21:47 +0100

    Make kotlin files

---
+++ app/src/main/java/com/quizarena/options/OptionsActivity.kt
@@ -0,0 +1,14 @@
+package com.quizarena.options
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.quizarena.R
+
+class OptionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_options)
+    }
+}
