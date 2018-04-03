ef06d3e8741d911739b77611cd9880782524e6f7
Milan Höllner <milan.hoellner@gmail.com>
2018-04-03 16:31:46 +0200

    WIP: validate session creation form

--- app/src/main/java/com/quizarena/sessions/creation/CreateSessionActivity.kt
+++ app/src/main/java/com/quizarena/sessions/creation/CreateSessionActivity.kt
@@ -15,11 +15,44 @@ class CreateSessionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_session)
 
         initializeCategories()
+
+        activity_create_session_submit_button.setOnClickListener {
+            // check whether input is valid
+            if (creationFormIsValid()) {
+                // do some create session request here
+
+                val name = activity_create_session_form_name.text.toString()
+                val category = activity_create_session_form_category.selectedItem.toString()
+                val duration = activity_create_session_form_duration.text.toString().toInt()
+
+                val api = CreateSessionApi()
+                api.createSession(name, category, duration)
+            }
+        }
     }
 
-    fun initializeCategories() {
+    /**
+     * initializes the dropdown with all categories from the API
+     */
+    private fun initializeCategories() {
         val array = CategoryApi().getAllCategories()
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, array)
         activity_create_session_form_category.adapter = adapter
+    }
+
+    /**
+     * validates the input of the create session form and displays error messages if necessary
+     * TODO: display error messages
+     */
+    private fun creationFormIsValid() : Boolean {
+        if (activity_create_session_form_name.text.isEmpty())
+            return false
+        if (activity_create_session_form_name.text.isBlank())
+            return false
+        if (activity_create_session_form_duration.text.isEmpty())
+            return false
+        if (activity_create_session_form_duration.text.toString().toIntOrNull() == null)
+            return false
+        return true
     }
 }
---
+++ app/src/main/java/com/quizarena/sessions/creation/CreateSessionApi.kt
@@ -0,0 +1,13 @@
+package com.quizarena.sessions.creation
+
+class CreateSessionApi {
+
+    var state: String = ""
+        private set
+    /**
+     * MOCK UP
+     */
+    fun createSession(name: String, category: String, duration: Int) {
+
+    }
+}
