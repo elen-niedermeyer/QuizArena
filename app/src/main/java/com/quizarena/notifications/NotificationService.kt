d00dfb3e0c6ebabe9f2af07bd5aa9510dadb9811
Miklas Boskamp <mboskamp@t-online.de>
2018-04-12 15:00:43 +0200

    Add message handling services

---
+++ app/src/main/java/com/quizarena/notifications/NotificationService.kt
@@ -0,0 +1,16 @@
+package com.quizarena.notifications
+
+import android.util.Log
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class NotificationService : FirebaseMessagingService() {
+
+    val TAG = "Service"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        Log.d(TAG, "From: " + remoteMessage!!.from)
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.notification.body!!)
+    }
+}
