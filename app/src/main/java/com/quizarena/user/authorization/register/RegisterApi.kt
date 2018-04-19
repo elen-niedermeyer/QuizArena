2c6ec7a4cf3335d5dc55f230eb435a0817f1542d
Elen Niedermeyer <elen.niedermeyer@posteo.de>
2018-04-19 16:52:20 +0200

    WIP: register api

--- app/src/main/java/com/quizarena/user/authorization/register/RegisterApi.kt
+++ app/src/main/java/com/quizarena/user/authorization/register/RegisterApi.kt
@@ -1,30 +1,37 @@
-package com.quizarena.user.authorization.register
-
 import android.content.Context
+import com.quizarena.ApiErrors
+import com.quizarena.R
+import org.jetbrains.anko.doAsync
 
 class RegisterApi {
 
     var state: String = ""
         private set
 
-    // TODO: handle responses (errors, id not unique, success --> go to main menu)
-    // TODO: set users display name
-    /*fun executeRegisterRequest(context: Context, name: String, password: String, displayName: String) : Boolean {
-        doAsync {
-            val response = khttp.post(
-                    url = context.getString(R.string.baseurl) + context.getString(R.string.user_endpoint) + name,
-                    data = mapOf(context.getString(R.string.api_parameter_password) to password))
-        }
-    }*/
-
+    // TODO: register with displayName
+    // TODO: rules for passwords
     // Mocking Request for easier debugging
     fun executeRegisterRequest(context: Context, name: String, password: String, displayName: String): Boolean {
-        if (name == "Test") {
+        var statusCode = 0
+        var responseState = ""
+
+        doAsync {
+            val response = khttp.post(
+                    url = context.getString(R.string.baseurl) + context.getString(R.string.endpoint_user_specific, name),
+                    data = mapOf(context.getString(R.string.endpoint_user_param_password) to password))
+            statusCode = response.statusCode
+            responseState = response.text
+        }
+
+        if (statusCode == 200) {
+            // register was successful
             return true
+
         } else {
-            state = "Ein Fehler ist aufgetreten"
+            // register was not successful
+            // parse error
+            this.state = ApiErrors.getUserError(context, responseState)
             return false
         }
     }
 }
-
